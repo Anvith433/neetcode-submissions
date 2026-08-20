6dcf1baa9f839d69fd5b82eb4bb8bc01f3ed462a
Anvith433 <pes1ug23cs093@pesu.pes.edu>
2026-08-20 21:59:02 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) 
+    {
+       HashMap<Integer,Integer> map=new HashMap<>();
+       boolean isDuplicate=false;
+       for(int i=0;i<nums.length;i++)
+       {
+        map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+       }
+       for(int key:map.keySet())
+       {
+        if(map.get(key)>1)
+        {
+            isDuplicate=true;
+        }
+       }
+       return isDuplicate;
+
+
+    }
+}
